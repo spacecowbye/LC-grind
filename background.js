ce4985245bce9abb3cca45b49b8263a9621073e6
Vatsal <vatsal.shah2304@gmail.com>
2025-02-01 13:48:13 +0530

    final changes before publishing

--- background.js
+++ background.js
@@ -17,9 +17,7 @@ const userState = {
     difficulty: undefined,
   },
   lastSubmissionDate: new Date(0),
-  lastAttemptedUrl: null,
   submitListenerActive: true,
-  lastAttemptedUrlListenerActive: false,
 };
 
 function onMessageReceived(message, sender, sendResponse) {
@@ -134,67 +132,64 @@ async function setRedirectRule(url) {
   }
 }
 
-    const injectSuccessModal = async (tabId) => {
-        const injectedScripts = await chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        func: () => Boolean(window._successModalInjected),
-        });
-    
-        if (!injectedScripts[0].result) {
-        await chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ["successModal.js"],
-        });
-    
-        
-        await chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            func: () => {
-            window._successModalInjected = true;
-            },
-        });
-        }
-    
-        
-        const {currentStreak = 0} = await chrome.storage.local.get('currentStreak');
-        await chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            func: (streak) => {
-            createModal(streak);
-            },
-            args: [currentStreak]  
-        });
-    };
+const injectSuccessModal = async (tabId) => {
+  const injectedScripts = await chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    func: () => Boolean(window._successModalInjected),
+  });
 
-    const injectFailureModal = async (tabId) => {
-        const injectedScripts = await chrome.scripting.executeScript({
-          target: { tabId: tabId },
-          func: () => Boolean(window._failureModalInjected),
-        });
-      
-        if (!injectedScripts[0].result) {
-          await chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ["failureModal.js"],
-          });
-    
-          await chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            func: () => {
-              window._failureModalInjected = true;
-            },
-          });
-        }
-      
-        
-        await chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            func: () => {
-              createFailureModal();
-            },
-        });
-    };
-    
+  if (!injectedScripts[0].result) {
+    await chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ["successModal.js"],
+    });
+
+    await chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      func: () => {
+        window._successModalInjected = true;
+      },
+    });
+  }
+
+  const { currentStreak = 0 } = await chrome.storage.local.get("currentStreak");
+  await chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    func: (streak) => {
+      createModal(streak);
+    },
+    args: [currentStreak],
+  });
+};
+
+const injectFailureModal = async (tabId) => {
+  const injectedScripts = await chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    func: () => Boolean(window._failureModalInjected),
+  });
+
+  if (!injectedScripts[0].result) {
+    await chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ["failureModal.js"],
+    });
+
+    await chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      func: () => {
+        window._failureModalInjected = true;
+      },
+    });
+  }
+
+  await chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    func: () => {
+      createFailureModal();
+    },
+  });
+};
+
 function updateUserState(title, fullLink, difficulty) {
   userState.potd_solved = false;
   userState.problem.difficulty = difficulty;
@@ -233,84 +228,89 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 });
 
 const checkIfUserSolvedProblem = async (details) => {
-    if (userState.potd_solved) {
-        return;
-    }
+  if (userState.potd_solved) {
+    return;
+  }
 
-    let currentURL = "";
+  let currentURL = "";
+  try {
+    const [activeTab] = await new Promise((resolve) => {
+      chrome.tabs.query({ active: true, currentWindow: true }, resolve);
+    });
+
+    currentURL = activeTab.url;
+    console.log(currentURL);
+  } catch (error) {
+    console.error("Error getting active tab:", error);
+    return;
+  }
+  const problemUrl = userState.problem.url;
+  const sameUrl =
+    problemUrl === currentURL || problemUrl + "description/" === currentURL;
+
+  if (!sameUrl) {
+    return;
+  }
+
+  if (userState.submitListenerActive) {
+    userState.submitListenerActive = false;
+    chrome.webRequest.onCompleted.removeListener(checkIfUserSolvedProblem);
+    console.log("Submit listener turned off");
+  }
+  const url = details.url;
+
+  if (isLeetcodeUrl(url) && url.includes("/check/")) {
     try {
-        const [activeTab] = await new Promise((resolve) => {
-            chrome.tabs.query({ active: true, currentWindow: true }, resolve);
-        });
-
-        currentURL = activeTab.url;
-        console.log(currentURL);
-    } catch (error) {
-        console.error("Error getting active tab:", error);
-        return;
-    }
-    const problemUrl = userState.problem.url;
-    const sameUrl =
-        problemUrl === currentURL || problemUrl + "description/" === currentURL
-
-    if (!sameUrl) {
-        return;
-    }
-
-    if (userState.submitListenerActive) {
-        userState.submitListenerActive = false;
-        chrome.webRequest.onCompleted.removeListener(checkIfUserSolvedProblem)
-        console.log("Submit listener turned off");
-
-    }
-    const url = details.url;
-
-    if (isLeetcodeUrl(url) && url.includes('/check/')) {
-        try {
-            const response = await fetch(details.url)
-            const data = await response.json()
-            if (data.state === "STARTED" || data.state === "PENDING") {
-                if (!userState.submitListenerActive) {
-                    userState.submitListenerActive = true
-                    chrome.webRequest.onCompleted.addListener(checkIfUserSolvedProblem, {
-                        urls: ["*://leetcode.com/submissions/detail/*/check/"]
-                    })
-                }
-                return
-            }
-            if (data.status_msg !== "Accepted") {
-                const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-                injectFailureModal(tab.id);
-                return
-            }
-            if (
-                data.status_msg === "Accepted" &&
-                data.state === "SUCCESS" &&
-                !data.code_answer
-            ) {
-                await updateStreak();
-                userState.leetcodeProblemSolved = true
-                chrome.declarativeNetRequest.updateDynamicRules({
-                    removeRuleIds: [RULE_ID]
-                })
-                chrome.webRequest.onCompleted.removeListener(checkIfUserSolvedProblem)
-                chrome.storage.local.get(["problem"], (data) => {
-                    if (data.problem) {
-                        data.problem.status = true; // Update status here
-                        chrome.storage.local.set({ problem: data.problem });
-                    } else {
-                        console.log("Problem not found in storage");
-                    }
-                });
-                const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-                await injectSuccessModal(tab.id);
-            }
-        } catch (error) {
-            console.error("Error:", error)
+      const response = await fetch(details.url);
+      const data = await response.json();
+      if (data.state === "STARTED" || data.state === "PENDING") {
+        if (!userState.submitListenerActive) {
+          userState.submitListenerActive = true;
+          chrome.webRequest.onCompleted.addListener(checkIfUserSolvedProblem, {
+            urls: ["*://leetcode.com/submissions/detail/*/check/"],
+          });
         }
+        return;
+      }
+      if (data.status_msg !== "Accepted") {
+        const [tab] = await chrome.tabs.query({
+          active: true,
+          currentWindow: true,
+        });
+        injectFailureModal(tab.id);
+        return;
+      }
+      if (
+        data.status_msg === "Accepted" &&
+        data.state === "SUCCESS" &&
+        !data.code_answer
+      ) {
+        await updateStreak();
+        userState.leetcodeProblemSolved = true;
+        await chrome.storage.local.set({ lastSubmission: Date.now() });
+        chrome.declarativeNetRequest.updateDynamicRules({
+          removeRuleIds: [RULE_ID],
+        });
+        chrome.webRequest.onCompleted.removeListener(checkIfUserSolvedProblem);
+        chrome.storage.local.get(["problem"], (data) => {
+          if (data.problem) {
+            data.problem.status = true; // Update status here
+            chrome.storage.local.set({ problem: data.problem });
+          } else {
+            console.log("Problem not found in storage");
+          }
+        });
+        const [tab] = await chrome.tabs.query({
+          active: true,
+          currentWindow: true,
+        });
+        await injectSuccessModal(tab.id);
+      }
+    } catch (error) {
+      console.error("Error:", error);
     }
-
-}
+  }
+};
 
 function createMidnightAlarm() {
   const now = new Date();
@@ -399,23 +399,31 @@ async function setConstants() {
   await chrome.storage.local.set({ lastSubmission: new Date(0) });
 }
 chrome.runtime.onInstalled.addListener(async () => {
-  setConstants();
+  await setConstants();
   createMidnightAlarm();
   updateStorage();
-  //tryResetStreak();
+  tryResetStreak();
 });
 
 async function tryResetStreak() {
-  const result = await chrome.storage.local.get("lastSubmission");
-  const lastSubmissionDate = new Date(Number(result.lastSubmission));
-  const yesterday = new Date().getDate() - 1;
-
-  if (lastSubmissionDate.getDate() < yesterday) {
-    chrome.storage.local.set({ currentStreak: 0 });
+    const result = await chrome.storage.local.get("lastSubmission");
+    const lastSubmissionDate = new Date(Number(result.lastSubmission));
+    
+    // Get current date & time
+    const now = new Date();
+    
+    // Set both to midnight to only compare full days
+    const lastSubmissionMidnight = new Date(lastSubmissionDate.setHours(0, 0, 0, 0));
+    const yesterdayMidnight = new Date(now.setHours(0, 0, 0, 0) - 86400000); // 86400000ms = 1 day
+  
+    if (lastSubmissionMidnight < yesterdayMidnight) {
+      await chrome.storage.local.set({ currentStreak: 0 });
+    }
+  
+    console.log("From Try Reset Streak");
+    console.log({ lastSubmissionDate, yesterdayMidnight });
   }
-  console.log("From Try Reset Streak");
-  console.log(lastSubmissionDate, yesterday);
-}
+  
 
 chrome.alarms.onAlarm.addListener(async (alarm) => {
   if (alarm.name === "midnightAlarm") {
@@ -423,7 +431,7 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
     try {
       await updateStorage();
       await createMidnightAlarm();
-      //await tryResetStreak();
+      await tryResetStreak();
     } catch (error) {
       console.error("Error handling midnight alarm:", error);
     }
